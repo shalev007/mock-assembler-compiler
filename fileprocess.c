4b33b925418452d50ca46ef7b78fe43869c8ff8e
David Sariel <davidsa@openu.ac.il>
2019-07-27 13:39:57 -0400

    added process file

--- fileprocess.c
+++ fileprocess.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
+#include <stdbool.h>
+
+ 
 #define MAX_LINE_INPUT 81
+#define COMMENT_SIGN ';'
+
+bool is_comment(char line []);
 
 void process_file(char *filename)
 {
@@ -12,9 +17,21 @@ void process_file(char *filename)
 	if((file = fopen(filename, "r"))) {
 		/* read each line */
 		while(fgets(line, MAX_LINE_INPUT, file)){
-			puts(line);
+			/* skips comment lines */
+			if(!is_comment(line)) {
+				/*symbol process*/
+			}
 		}
 	} else {
 		printf("%s does not exist...\n", filename);
 	}
+}
+
+bool is_comment(char line[])
+{
+	if(((char) line[0]) == COMMENT_SIGN) {
+		return true;
+	}
+
+	return false;
 }
