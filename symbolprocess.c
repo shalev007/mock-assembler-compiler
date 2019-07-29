9ba934bf327743d2b31ea5c586fc0ef8d0a7bd47
David Sariel <davidsa@openu.ac.il>
2019-07-29 11:51:49 -0400

    refactored and fixed bool convertLineToWordsArray function

--- symbolprocess.c
+++ symbolprocess.c
@@ -3,17 +3,19 @@
 
 char ** convertLineToWordsArray(char *line);
 
+bool is_external(char word[]);
+
+bool is_entry(char word[]);
+
 void process_symbol(char *line)
 {
-	int i;
+	int i = 0;
 	char ** words = convertLineToWordsArray(line);
-
 	/* loop through each word in the line*/
 	while(words[i]) {
 		printf("words[%d]: %s\n", i, words[i]);
 		i++;
 	}
-	printf("%s\n", line);
 	return;
 }
 
@@ -24,23 +26,57 @@ char ** convertLineToWordsArray(char *line)
 {
 	char * nextWord;
 	char ** wordArray;
-	int i;
+	int i = 0;
+	char *lineClone;
 	wordArray = NULL;
-	nextWord = strtok(line, WORD_SPLIT_TOKENS);
+	/* allocate space same as line */
+	lineClone = (char *) malloc((strlen(line)+1)* sizeof(char));
+	/* clone line */
+	strcpy(lineClone, line);
+	/*grabs the next word from the string (without whitespaces)*/
+	nextWord = strtok(lineClone, WORD_SPLIT_TOKENS);
 
 	while (nextWord != NULL)
 	{
+		/*reallocating space from the next word in the array*/
 		wordArray = (char **) realloc(wordArray, (i + 1) * sizeof(char *));
+		/* allocate space for the incoming word*/
 		wordArray[i] = (char *) malloc(sizeof(nextWord) * sizeof(char));
+		/* actually setting the word */
 		wordArray[i] = nextWord;
+		/* grabing the next word from the string */
 		nextWord = strtok (NULL, WORD_SPLIT_TOKENS);
 		i++;
 	}
 
 	/* add NULL to last index to indicate the end of the array */
-	wordArray = (char **) realloc(wordArray, (i + 1) * sizeof(char *));
-	wordArray[++i] = (char *) malloc(sizeof(nextWord) * sizeof(char));
+	wordArray = (char **) realloc(wordArray, i * sizeof(char *));
+	wordArray[i] = (char *) malloc(sizeof(nextWord) * sizeof(char));
 	wordArray[i] = NULL;
 
 	return wordArray;
+}
+
+/**
+ * checks weather sign is the .external key word
+ */
+bool is_external(char word[])
+{
+	if(strcmp(EXTERNAL_SIGN, word) == 0) {
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ * checks weather sign is the .entry key word
+ */
+bool is_entry(char word[])
+{
+	if(strcmp(ENTRY_SIGN, word) == 0) {
+		return true;
+	}
+
+	return false;
 }
--- symbolprocess.h
+++ symbolprocess.h
@@ -2,5 +2,8 @@
 #include <string.h>
 
 #define WORD_SPLIT_TOKENS " \n"
+#define EXTERNAL_SIGN ".extern"
+#define ENTRY_SIGN ".entry"
+
 
 void process_symbol(char *line);
