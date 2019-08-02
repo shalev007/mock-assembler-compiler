50609c3b5f879e30425bbb8bab49c66eec60bb3f
David Sariel <davidsa@openu.ac.il>
2019-08-02 12:11:49 -0400

    WIP calculate and verificate command space allocation

--- instructionsregister.c
+++ instructionsregister.c
@@ -2,32 +2,131 @@
 
 int _IC = IC_DEFAULT_VALUE;
 
-const char * command_list[] = {
-	"mov", /* id: 0 */
-	"cmp", /* id: 1 */
-	"add", /* id: 2 */
-	"sub", /* id: 3 */
-	"not", /* id: 4 */
-	"clr", /* id: 5 */
-	"lea", /* id: 6 */
-	"inc", /* id: 7 */
-	"dec", /* id: 8 */
-	"jmp", /* id: 9 */
-	"bne", /* id: 10 */
-	"red", /* id: 11 */
-	"prn", /* id: 12 */
-	"jsr", /* id: 13 */
-	"rts", /* id: 14 */
-	"stop" /* id: 15 */ 
+/* 0,1,2,3 */
+AddressingMode allAddressModes[] = {IMMEDIATE, DIRECT, INDEXED, REGISTER};
+/* 1,2,3 */
+AddressingMode noImmediateAddressModes[] = {DIRECT, INDEXED, REGISTER};
+/* 1,2 */
+AddressingMode drctAndIndxAddressModes[] = {DIRECT, INDEXED};
+/* 1,3 */
+AddressingMode drctAndRegsAddressModes[] = {DIRECT, REGISTER};
+
+/**
+* Collection of all the commands,
+* with their allowed addressing modes
+* for source operand and destination operand
+* as mentioned in assigment notebook.
+*/
+Command command_list[16] = {
+	{
+		0,
+		"mov",
+		allAddressModes,
+		noImmediateAddressModes
+	},
+	{
+		1,
+		"cmp",
+		allAddressModes,
+		allAddressModes
+	},
+	{
+		2,
+		"add",
+		allAddressModes,
+		noImmediateAddressModes
+	},
+	{
+		3,
+		"sub",
+		allAddressModes,
+		noImmediateAddressModes
+	},
+	{
+		4,
+		"not",
+		NULL,
+		noImmediateAddressModes
+	},
+	{
+		5,
+		"clr",
+		NULL,
+		noImmediateAddressModes
+	},
+	{
+		6,
+		"lea",
+		drctAndIndxAddressModes,
+		noImmediateAddressModes
+	},
+	{
+		7,
+		"inc",
+		NULL,
+		noImmediateAddressModes
+	},
+	{
+		8,
+		"dec",
+		NULL,
+		noImmediateAddressModes
+	},
+	{
+		9,
+		"jmp",
+		NULL,
+		drctAndRegsAddressModes
+	},
+	{
+		10,
+		"bne",
+		NULL,
+		drctAndRegsAddressModes
+	},
+	{
+		11,
+		"red",
+		NULL,
+		noImmediateAddressModes
+	},
+	{
+		12,
+		"prn",
+		NULL,
+		allAddressModes
+	},
+	{
+		13,
+		"jsr",
+		NULL,
+		drctAndRegsAddressModes
+	},
+	{
+		14,
+		"rts",
+		NULL,
+		NULL
+	},
+	{
+		15,
+		"stop",
+		NULL,
+		NULL
+	}
 };
+
+bool validate_command(char ** commandline);
+
+bool validate_command_addressing_mode(char * name, char * dest, char * src);
 
 signed int get_command_id(char * command)
 {
 	int count = 16;
 
 	while(count--) {
-		if(strcmp(command_list[count], command) == 0) {
-			return count;
+		if(strcmp(command_list[count].name, command) == 0) {
+			return command_list[count].id;
 		}
 	}
 	return -1;
@@ -45,10 +144,56 @@ bool is_command(char * command)
 int calculate_command_space(char ** commandline)
 {
 	int i = 0;
+	int numOfOperands = 0;
+	char * commandName;
+	char * dest = NULL;
+	char * src = NULL;
 
+	/* validate that command exists */
+	if(!validate_command(commandline)) {
+		return 0;
+	}
+
+	commandName = commandline[0];
+
+	/* calculate number of operands */
 	while(commandline[i]) {
-		printf("%s\n", commandline[i]);
+		if(strcmp(commandline[i], commandName) != 0) {
+			numOfOperands++;
+		}
 		i++;
 	}
+
+	/* assign destination and source operands */
+	if(numOfOperands == 1) {
+		dest = commandline[1];
+
+	} else if(numOfOperands == 2) {
+		dest = commandline[2];
+		/* create the src without the ',' sign*/
+		src = (char *) malloc(sizeof(commandline[1]) * sizeof(char));
+        strncat(src, commandline[1], sizeof(commandline[1])-1);
+	}
+
+	/* validate that command can use these addressing modes */
+	if (!validate_command_addressing_mode(commandName, dest, src)) {
+		return 0;
+	}
+
 	return 0;
+}
+
+bool validate_command(char ** commandline)
+{
+	if(!is_command(commandline[0])) {
+		printf("%s is not a valid command...\n", commandline[0]); /*TODO: continue and add error flag to true and file read line number*/
+		return false;
+	}
+
+	return true;
+}
+
+bool validate_command_addressing_mode(char * name, char * dest, char * src)
+{
+	return true;
 }
--- instructionsregister.h
+++ instructionsregister.h
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #define IC_DEFAULT_VALUE 100
+
+/* type of addressing mode */
+typedef enum addressingMode {
+	IMMEDIATE,
+	DIRECT,
+	INDEXED,
+	REGISTER
+} AddressingMode;
+
+/* command struct */
+typedef struct {
+	unsigned id;
+	char * name;
+	AddressingMode * allowedAddressingsSrc;
+	AddressingMode * allowedAddressingsDest;
+}  Command;
 
 signed int get_command_id(char * command);
 
